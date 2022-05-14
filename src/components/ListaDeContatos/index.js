0c1c853136877a404cae36af9eee22e264fce4b6
Cleiton Daniel (Kaonami) <danielsdev77@gmail.com>
2022-05-14 15:22:31 -0300

    Referencias da Api adicionadas

--- src/components/ListaDeContatos/index.js
+++ src/components/ListaDeContatos/index.js
@@ -1,22 +1,35 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import api from "../../services/api"
 import "./style.css"
 
 
 function ListaDeContatos() {
+
+    const [contatos, setContato] = useState()
+
+    useEffect(() => {
+        api
+            .get("/contatos")
+            .then((response) => setContato(response.data))
+            .catch((err) => {
+                console.error("ops! ocorreu um erro" + err);
+            });
+    }, []);
+
     return (
         <div className="container">
             <div className="box">
 
                 <h3 className="titulo">Lista de contatos</h3>
 
-                <form action="" className="form">
+                <form action="http://localhost:4000/contatos" method="post" className="form">
                     {/* <!-- Nome --> */}
                     <div className="inputBox">
-                        <input type="text" name="nome" id="nome" placeholder="Nome" className="inputStyle"/>
+                        <input type="text" name="nome" id="nome" placeholder="Nome" className="inputStyle" />
                     </div>
                     {/* <!-- Numero --> */}
                     <div className="inputBox">
-                        <input type="text" name="numero" id="numero" placeholder="Número" className="inputStyle"/>
+                        <input type="text" name="numero" id="numero" placeholder="Número" className="inputStyle" />
                     </div>
                     <div className="actions">
                         <button type="submit">Salvar</button>
@@ -24,23 +37,20 @@ function ListaDeContatos() {
                 </form>
 
                 <form action="" className="form" id="searchBox">
-                    <input type="search" placeholder="Procure um contato" id="search"/>
-                        <button type="submit">Procurar</button>
+                    <input type="search" placeholder="Procure um contato" id="search" />
+                    <button type="submit">Procurar</button>
                 </form>
 
                 <div className="contatos">
-                    <div className="contato">
-                        <h4>Cleiton Daniel</h4>
-                        <h4>-</h4>
-                        <h4>99 9999-9999</h4>
-                    </div>
-                    <div className="contato"></div>
-                    <div className="contato"></div>
-                    <div className="contato"></div>
-                    <div className="contato"></div>
-                    <div className="contato"></div>
-                </div>
 
+                    {contatos?.map(contato => (
+                        <div className="contato" key={contato.id}>
+                        <h4>{contato.nome}</h4>
+                        <h4>{contato.numero}</h4>
+                        <a href={`http://localhost:4000/contatos/delete/${contato.id}`}> <button className="delete-button">X</button></a>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     )
